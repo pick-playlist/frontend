3e79589136b545efe9300f833b14175dbd44e355
yeri <rlafl7942@naver.com>
2024-02-22 15:19:35 +0900

    feat: set SignupPage layout

--- src/pages/start/signup/page.jsx
+++ src/pages/start/signup/page.jsx
@@ -1,5 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../../../assets/react.svg";
+import { Button, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function SignupPage() {
-  return <div>SignupPage</div>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  return (
+    <Container fluid className="d-flex justify-content-center min-vh-100">
+      <div
+        style={{
+          backgroundColor: "white",
+          marginTop: "10vh",
+          height: "100%",
+          width: "60vw",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <h1>PICKPL</h1>
+        <img src={logo} style={{ width: 250, height: 250 }} />
+        <Form>
+          <Form.Control
+            type="text"
+            value={nickname}
+            onChange={(e) => {
+              setNickname(e.target.value);
+            }}
+            placeholder="닉네임을 입력해주세요."
+            style={{
+              width: "20vw",
+              height: "6vh",
+              backgroundColor: "#F7DFFF",
+              borderWidth: 0,
+              margin: 20,
+            }}
+          />
+          <Form.Control
+            type="text"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="이메일을 입력해주세요."
+            style={{
+              width: "20vw",
+              height: "6vh",
+              backgroundColor: "#F7DFFF",
+              borderWidth: 0,
+              margin: 20,
+            }}
+          />
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="비밀번호를 입력해주세요."
+            style={{
+              width: "20vw",
+              height: "6vh",
+              backgroundColor: "#F7DFFF",
+              borderWidth: 0,
+              margin: 20,
+            }}
+          />
+        </Form>
+        <Button
+          onClick={() => navigate("/main")}
+          variant="outline-dark"
+          style={{ width: "16vw", height: "6vh", margin: 5 }}
+        >
+          SIGN UP
+        </Button>
+        <p
+          style={{ textDecoration: "underline", cursor: "pointer" }}
+          onClick={() => navigate("/signup")}
+        >
+          회원가입
+        </p>
+      </div>
+    </Container>
+  );
 }
