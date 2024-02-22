f721c0bba3a0e169b474f4232bbbab110087bab0
yeri <rlafl7942@naver.com>
2024-02-22 16:15:27 +0900

    feat: set MainPage layout

--- src/pages/main/page.jsx
+++ src/pages/main/page.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import { Button, Container } from "react-bootstrap";
+import logo from "../../assets/react.svg";
+import { List } from "react-bootstrap-icons";
 
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <Container
+      fluid
+      className="d-flex flex-column align-items-center min-vh-100"
+    >
+      <Container className="mt-3">
+        <List size="40" style={{ cursor: "pointer" }} />
+      </Container>
+      <div
+        style={{
+          backgroundColor: "white",
+          height: "100%",
+          marginTop: "10vh",
+          width: "60vw",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <img src={logo} style={{ width: 250, height: 250 }} />
+        <h1>PICKPL</h1>
+        <Button
+          onClick={() => navigate("/main")}
+          variant="outline-dark"
+          style={{ width: "16vw", height: "6vh", marginTop: "5vh" }}
+        >
+          방 만들기
+        </Button>
+        <Button
+          onClick={() => navigate("/main")}
+          variant="outline-dark"
+          style={{ width: "16vw", height: "6vh", margin: "5vh" }}
+        >
+          참여하기
+        </Button>
+      </div>
+    </Container>
+  );
 }
