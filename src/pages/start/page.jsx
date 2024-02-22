dabba89846402b96688bd2c032ebef89209430f6
yeri <rlafl7942@naver.com>
2024-02-22 14:55:01 +0900

    chore: set StartPage layout

--- src/pages/start/page.jsx
+++ src/pages/start/page.jsx
@@ -1,27 +1,46 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { setIsLoggedInTrue, setIsLoggedInFalse } from "~/store/reducers/user";
+import logo from "../../assets/react.svg";
+import { Button, Container } from "react-bootstrap";
 
 export default function StartPage() {
-  const userObj = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
-  console.log("user: ", userObj.isLoggedIn);
-
   return (
-    <div>
-      <p>redux test in startPage</p>
-
-      <button
-        onClick={() => {
-          dispatch(setIsLoggedInTrue());
+    <Container fluid className="d-flex justify-content-center min-vh-100">
+      <div
+        style={{
+          backgroundColor: "white",
+          marginTop: "10vh",
+          height: "100%",
+          width: "60vw",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          alignItems: "center",
         }}
       >
-        make true
-      </button>
-      <button>make false</button>
-
-      <p>isLoggedIn : {userObj.isLoggedIn}</p>
-    </div>
+        <h1>PICKPL</h1>
+        <img
+          src={logo}
+          style={{ width: 250, height: 250, marginBottom: "10vh" }}
+        />
+        <Button
+          variant="outline-dark"
+          style={{ width: "20vw", height: "7vh", margin: 5 }}
+        >
+          로그인
+        </Button>
+        <Button
+          variant="outline-dark"
+          style={{ width: "20vw", height: "7vh", margin: 5 }}
+        >
+          회원가입
+        </Button>
+        <Button
+          variant="outline-dark"
+          style={{ width: "20vw", height: "7vh", margin: 5 }}
+        >
+          비회원으로 이용하기
+        </Button>
+      </div>
+    </Container>
   );
 }
