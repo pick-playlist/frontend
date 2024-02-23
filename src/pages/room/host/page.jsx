ce1e29531dfa12799364568a80842a8e478ae18f
yeri <rlafl7942@naver.com>
2024-02-23 14:17:13 +0900

    feat: set currentMusicInfo in HostRoomPage

--- src/pages/room/host/page.jsx
+++ src/pages/room/host/page.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 import YoutubePlayer from "~/components/youtubePlayer/YoutubePlayer";
 
 export default function RoomHostPage() {
@@ -12,10 +11,93 @@ export default function RoomHostPage() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "column",
         width: "100%",
       }}
     >
       <YoutubePlayer video={video} />
+      <div
+        style={{
+          width: "100vw",
+          padding: 15,
+        }}
+      >
+        <h3>승택님의 잼잼잼잼~~~~</h3>
+        <div>
+          <h5>현재 재생중인 음악 🎶</h5>
+          <p>비비 - 밤양갱</p>
+        </div>
+        <div>현재 3명이 참여중</div>
+        <div
+          style={{
+            display: "flex",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              color: "#fff",
+              width: "40px",
+              height: "40px",
+              backgroundImage: "linear-gradient(#3d7fff, #4b5dff)",
+              border: "1px solid #3961d9",
+              borderRadius: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              fontWeight: 700,
+              display: "flex",
+              overflow: "hidden",
+              boxShadow: "0 1px 4px rgba(74, 58, 255, .26)",
+              zIndex: 2,
+            }}
+          >
+            YR
+          </div>
+          <div
+            style={{
+              color: "#fff",
+              width: "40px",
+              height: "40px",
+              backgroundImage: "linear-gradient(#3d7fff, #4b5dff)",
+              border: "1px solid #3961d9",
+              borderRadius: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              fontWeight: 700,
+              display: "flex",
+              overflow: "hidden",
+              boxShadow: "0 1px 4px rgba(74, 58, 255, .26)",
+              position: "absolute", // 겹치는 div에 position: absolute; 추가
+              top: 0, // 원하는 위치로 조정
+              left: "30px", // 원하는 위치로 조정
+              zIndex: 1,
+            }}
+          >
+            YH
+          </div>
+          <div
+            style={{
+              color: "#fff",
+              width: "40px",
+              height: "40px",
+              backgroundImage: "linear-gradient(#3d7fff, #4b5dff)",
+              border: "1px solid #3961d9",
+              borderRadius: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              fontWeight: 700,
+              display: "flex",
+              overflow: "hidden",
+              boxShadow: "0 1px 4px rgba(74, 58, 255, .26)",
+              position: "absolute", // 겹치는 div에 position: absolute; 추가
+              top: 0, // 원하는 위치로 조정
+              left: "60px", // 원하는 위치로 조정
+            }}
+          >
+            ST
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
