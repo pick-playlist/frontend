bb0190aaefaf7e0aa9affa8912418e4961811b98
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-25 19:24:39 +0900

    feat: apply redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,36 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/user";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
 
-const rootReducer = combineReducers({
-  user: userReducer,
-});
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+import persistReducer from "redux-persist/es/persistReducer";
+
+const rootPersistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["user"],
+};
+
+const rootReducer = persistReducer(
+  rootPersistConfig,
+  combineReducers({
+    user: userReducer,
+  })
+);
 
 const store = configureStore({
   reducer: rootReducer,
 });
+
+const persistor = persistStore(store);
+export { store, persistor };
 
 export default store;
