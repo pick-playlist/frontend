f4f57f806507d71f46e7732f4b7e631662fd2151
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-25 18:06:14 +0900

    feat: apply user state in sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { X } from "react-bootstrap-icons";
 import logo from "../../assets/react.svg";
 import styled, { css, keyframes } from "styled-components";
 import { Accordion } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+const EMPTY_PLAYLIST = "플레이리스트가 비어 있습니다.";
 
 export default function Sidebar(props) {
+  const user = useSelector((state) => state.user.data);
+  const [profilePhoto, setProfilePhoto] = useState(logo);
+  const [nickname, setNickname] = useState("");
+  const [playlist, setPlaylist] = useState([]);
+  const [acceptPlaylist, setAcceptPlaylist] = useState([]);
+  const [rejectPlaylist, setRejectPlaylist] = useState([]);
+
+  useEffect(() => {
+    console.log("User data:", user);
+
+    if (user) {
+      setProfilePhoto(user.profilePhoto);
+      setNickname(user.nickname);
+      setPlaylist(user.playlist.data);
+      setAcceptPlaylist(user.acceptPlaylist.data);
+      setRejectPlaylist(user.rejectPlaylist.data);
+    }
+  }, []);
+
   return (
     <AnimationComponent>
       <div
@@ -30,7 +52,7 @@ export default function Sidebar(props) {
           onClick={() => props.setSidebarVisible(false)}
         />
         <div style={{ display: "flex", alignItems: "center" }}>
-          <img src={logo} style={{ flex: 1 }} />
+          <img src={profilePhoto} style={{ flex: 1, width: "10px" }} />
           <div
             style={{
               display: "flex",
@@ -39,22 +61,40 @@ export default function Sidebar(props) {
               marginLeft: "10%",
             }}
           >
-            <span style={{ color: "white" }}>예리</span>
+            <span style={{ color: "white" }}>{nickname}</span>
             <span style={{ color: "#C5C5C5" }}>노래 등록 성공률</span>
           </div>
         </div>
         <Accordion className="mt-3" defaultActiveKey="0">
           <Accordion.Item eventKey="0">
             <Accordion.Header>나의 플레이리스트</Accordion.Header>
-            <Accordion.Body>ㄴ이ㅏ러ㅣㄴㅇ라ㅓㅇ나ㅡ</Accordion.Body>
+            <Accordion.Body>
+              {playlist.length == 0
+                ? EMPTY_PLAYLIST
+                : playlist.map((pl) => {
+                    return <div key={pl}>{pl}</div>;
+                  })}
+            </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
             <Accordion.Header>수락된 플레이리스트</Accordion.Header>
-            <Accordion.Body>수락된 플레이리스트 목록입니다.</Accordion.Body>
+            <Accordion.Body>
+              {acceptPlaylist.length == 0
+                ? EMPTY_PLAYLIST
+                : acceptPlaylist.map((pl) => {
+                    return <div key={pl}>{pl}</div>;
+                  })}
+            </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
             <Accordion.Header>거절된 플레이리스트</Accordion.Header>
-            <Accordion.Body>거절된 플레이리스트 목록입니다.</Accordion.Body>
+            <Accordion.Body>
+              {rejectPlaylist.length == 0
+                ? EMPTY_PLAYLIST
+                : rejectPlaylist.map((pl) => {
+                    return <div key={pl}>{pl}</div>;
+                  })}
+            </Accordion.Body>
           </Accordion.Item>
         </Accordion>
       </div>
