f45bf638951f917e3e8c08abdebd41306145e1bc
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-26 16:52:55 +0900

    refactor: remove unneed line

--- src/pages/room/party/page.jsx
+++ src/pages/room/party/page.jsx
@@ -6,14 +6,5 @@ export default function RoomPartyPage() {
   const user = useSelector((state) => state.user.data);
   const room = useSelector((state) => state.room.data);
 
-  useEffect(() => {
-    if (user && room) {
-      console.log("user: ", user);
-      console.log("room:", room);
-    } else {
-      if (!user) console.error("user is null");
-      if (!room) console.error("room is null");
-    }
-  }, [room, user]);
   return <RoomInfo />;
 }
