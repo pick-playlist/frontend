bb491eedeb54969f0083e5cd054599ad2a8081eb
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-26 15:09:58 +0900

    feat(WIP): connect room API

--- src/pages/room/host/page.jsx
+++ src/pages/room/host/page.jsx
@@ -10,32 +10,18 @@ import styled, { keyframes } from "styled-components";
 import { getLinkInfo } from "~/lib/api/search";
 import { createMusic } from "~/lib/api/music";
 import { addMusicInPlaylist } from "~/lib/api/playlist";
+import RoomInfo from "~/components/roomInfo/roomInfo";
 
 export default function RoomHostPage() {
+  const location = useLocation();
   const video = {
     key: "mFbILexYSQg",
-  };
-
-  const clickAddButton = async (link, playlistId) => {
-    const linkInfoResp = await getLinkInfo(link);
-
-    const title = linkInfoResp.title;
-    const artist = "none";
-    // 아래 넣어주세용
-    const comment = "put comment";
-    const userId = "65d6f115b32758d2cd0559fd";
-
-    const musicResp = await createMusic(title, artist, comment, userId, link);
-    const createdMusicId = musicResp._id;
-
-    const playlistResp = await addMusicInPlaylist(createdMusicId, playlistId);
-    return playlistResp;
   };
 
   return (
     <div>
       <YoutubePlayer video={video} />
-      <RoomInfo />
+      <RoomInfo isHost={location.state.isCreateRoom} />
     </div>
   );
 }
