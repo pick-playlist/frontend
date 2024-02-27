18887e035f87485c2220c81fd70d05d145f4a6e9
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-27 13:10:34 +0900

    feat: add logout button

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import userIcon from "../../assets/user.png";
+import { persistor } from "~/store/store";
 
 export default function NavBar() {
   const navigate = useNavigate();
   const userObj = useSelector((state) => state.user.data);
+  const [user, setUser] = useState(null);
+
+  const onClickLogOut = () => {
+    persistor.purge(["user"]); // 'user' 데이터 삭제
+    setUser(null);
+    navigate("/");
+  };
+
+  useEffect(() => {
+    setUser(userObj);
+  }, [userObj]);
+
   return (
     <div
       style={{
@@ -18,37 +31,42 @@ export default function NavBar() {
         justifyContent: "flex-end",
       }}
     >
-      {userObj ? (
-        <div style={{ color: "white", display: "flex" }}>
-          {userObj.nickname}님
-          <img
-            src={userObj.profilePhoto ? userObj.profilePhoto : userIcon}
+      {user ? (
+        <>
+          <div style={{ color: "white", display: "flex" }}>
+            {userObj.nickname}님
+            <img
+              src={userObj.profilePhoto ? userObj.profilePhoto : userIcon}
+              style={{
+                marginLeft: "5px",
+                width: "25px",
+                height: "25px",
+                backgroundColor: "white",
+                borderRadius: 100,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                cursor: `${userObj.isMember ? "pointer" : ""}`,
+              }}
+              onClick={userObj.isMember ? () => navigate("/profile") : null} // Conditional onClick
+            />
+          </div>
+
+          <div
             style={{
-              marginLeft: "5px",
-              width: "25px",
-              height: "25px",
-              backgroundColor: "white",
-              borderRadius: 100,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              cursor: `${userObj.isMember ? "pointer" : ""}`,
+              marginLeft: "10px",
+              cursor: "pointer",
+              color: "white",
+              fontSize: "17px",
+              fontFamily: "IBMPlexSansKR-Regular",
             }}
-            onClick={userObj.isMember ? () => navigate("/profile") : null} // Conditional onClick
-          />
-        </div>
+            onClick={() => onClickLogOut()}
+          >
+            로그아웃
+          </div>
+        </>
       ) : (
-        <div
-          style={{
-            cursor: "pointer",
-            color: "white",
-            fontSize: "17px",
-            fontFamily: "IBMPlexSansKR-Regular",
-          }}
-          onClick={() => navigate("/login")}
-        >
-          로그인
-        </div>
+        ""
       )}
     </div>
   );
