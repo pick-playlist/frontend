97b1dda2eb930b2168d9138ca9bca0e471c0ae87
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-02-28 09:43:33 +0900

    feat: make rank of playlist

--- src/pages/visualization/page.jsx
+++ src/pages/visualization/page.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import ReactWordcloud from "react-wordcloud";
 import { ButtonInPages } from "~/components/styled/globalComponent";
+import { getMusicInfo } from "~/lib/api/music";
 import { getPlaylistInfo } from "~/lib/api/playlist";
 import { deleteRoom, deleteUserInRoom } from "~/lib/api/room";
 import { setRoomNull } from "~/store/reducers/room";
@@ -20,6 +21,7 @@ export default function Visualization() {
   const room = useSelector((state) => state.room.data);
 
   const [words, setWords] = useState([]);
+  const [ranks, setRanks] = useState();
 
   useEffect(() => {
     makeRank(room);
@@ -27,15 +29,55 @@ export default function Visualization() {
   }, []);
 
   const makeRank = async (room) => {
-    const roomId = room._id;
-    const acceptPlaylistId = room.acceptPlaylist._id;
-    const rejectPlaylistId = room.rejectPlaylist._id;
+    try {
+      const roomId = room._id;
+      const acceptPlaylistId = room.acceptPlaylist._id;
+      const rejectPlaylistId = room.rejectPlaylist._id;
 
-    console.log("pl ids: ", acceptPlaylistId, " ", rejectPlaylistId);
+      const acceptResp = await getPlaylistInfo(acceptPlaylistId);
+      const acceptMusics = acceptResp.musics;
 
-    getPlaylistInfo("ac: ", acceptPlaylistId);
+      const rejectResp = await getPlaylistInfo(rejectPlaylistId);
+      const rejectMusics = rejectResp.musics;
 
-    getPlaylistInfo("rj: ", rejectPlaylistId);
+      const allMusics = await acceptMusics.concat(rejectMusics);
+
+      let rankDiv = [];
+      console.log("all: ", allMusics);
+
+      if (allMusics.length > 0) {
+        allMusics.sort((a, b) => b.agree - a.agree);
+
+        let rank = 1;
+        let prevAgree = allMusics[0].agree;
+        const divLimit = 5;
+        let cnt = 0;
+        // console.log("prevAgree: ", prevAgree);
+
+        for (let i = 0; i < allMusics.length; i++) {
+          if (prevAgree > allMusics[i].agree) {
+            rank++;
+            prevAgree = allMusics[i].agree;
+          }
+
+          if (rank >= 4 || cnt >= divLimit) {
+            break;
+          }
+
+          cnt++;
+          rankDiv.push(
+            <div key={allMusics[i]._id}>
+              <p>{allMusics[i].title}</p>
+              <p>{rank}</p>
+            </div>
+          );
+        }
+
+        setRanks(rankDiv);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const countTags = async (tags) => {
@@ -80,6 +122,11 @@ export default function Visualization() {
 
   return (
     <div style={{ alignSelf: "start" }}>
+      <div className="mt-3">
+        <h2>동의를 많이 받은 음악</h2>
+        {ranks}
+      </div>
+
       <h2 style={{ fontFamily: "IBMPlexSansKR-Regular" }}>제안된 음악 태그</h2>
       <div>
         <ReactWordcloud words={words} />
